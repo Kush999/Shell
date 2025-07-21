4cc9f89799e6ab6d93d2719e18f7a62839ca612f
Kush Dhuvad <kushdhuvad9@gmail.com>
2025-07-21 12:02:31 -0400

    Added a comment

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -9,7 +9,7 @@
 # Usage:
 #   Please provide your github token and rest api to the script as input
 #
-#
+#This script will retrieve information from GitHub using the REST API.
 ################################
 
 if [ ${#@} -lt 2 ]; then
